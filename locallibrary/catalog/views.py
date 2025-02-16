2fa3eb4e7857039f81ff369fe9a63870f74943ec
kunn04 <javieraguero104@gmail.com>
2025-02-16 20:13:28 +0100

    Tutorial Part8 done
    Sessions were reviewed, added a visit counter to the web which adds visits every time a user reconnects to a database within the time his session is up.

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -22,13 +22,20 @@ def index(request):
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
 
+    # Number of visits to this view
+    
+    num_visits = request.session.get('num_visits', 0)
+    num_visits += 1
+    request.session['num_visits'] = num_visits
+    
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
-        'a_books':a_books,
-        'num_genders':num_genders
+        'a_books': a_books,
+        'num_genders': num_genders,
+        'num_visits': num_visits
     }
 
     # Render the HTML template index.html with the data in the context variable
