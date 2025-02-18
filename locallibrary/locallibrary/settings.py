cfd6d81959c0f8246f92c080ccd4cb4a18ea897c
luisnf5 <luisnun.fdez@gmail.com>
2025-02-18 22:29:37 +0100

    allowed_hosts

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-1qk@o!5v(%k9yf
 #DEBUG = False
 DEBUG = os.environ.get('DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -100,7 +100,7 @@ if 'TESTING' in os.environ:
     db_from_env = dj_database_url.config(default=POSTGRESQL_URL, conn_max_age=500)
 else:
     db_from_env = dj_database_url.config(default=NEON_URL, conn_max_age=500)
-    
+
 DATABASES['default'].update(db_from_env)
 
 # Password validation
