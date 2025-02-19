776bbecd6b11321da0d245233b91fd42a26c5dac
luisnf5 <luisnun.fdez@gmail.com>
2025-02-19 01:43:23 +0100

    yo que se

--- locallibrary/build.sh
+++ locallibrary/build.sh
@@ -1,14 +1,14 @@
 set -e
-
+echo "1"
 python3 manage.py flush --noinput
-
+echo "2"
 pip install -r requirements.txt
-
+echo "3"
 python3 manage.py makemigrations
 
 python3 manage.py migrate
 
-python3 manage.py collectstatic --noinput 
+python3 manage.py collectstatic --noinput
 
 python3 manage.py createsuperuser --noinput
 
